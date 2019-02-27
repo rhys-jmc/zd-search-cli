2dc3f569fb26f083aa534082884d0523efeb8efe
Rhys Gevaux <rhys@everproof.com>
2019-02-27 17:59:46 +1100

    Fixed incorrect exec callback parameter usage

--- tests/version.test.js
+++ tests/version.test.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 test("version", done => {
-  exec("node ./src/index.js --version", stdout => {
+  exec("node ./src/index.js --version", (_, stdout) => {
     expect(stdout).toEqual("1.0.0\n");
 
     done();
